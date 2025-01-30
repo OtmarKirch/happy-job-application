6df275dc91e7b5896f753ee65df2c241a59c988e
OtmarKirch <154664034+OtmarKirch@users.noreply.github.com>
2025-01-30 16:13:48 +0100

    Add get application methods

--- src/actions/index.ts
+++ src/actions/index.ts
@@ -2,24 +2,51 @@
 
 import { db } from '@/db'
 
-interface applicationProps {
+interface applicationData {
     company: string,
     jobTitle: string,
     applicationStatus: string
 }
 
-export async function createApplicationEntry(props: applicationProps) {
+export async function createApplicationEntry(data: applicationData) {
     'use server'
-    
   
     await db.application.create({
         data: {
-            company: props.company,
-            jobTitle: props.jobTitle,
-            applicationStatus: props.applicationStatus
+            company: data.company,
+            jobTitle: data.jobTitle,
+            applicationStatus: data.applicationStatus
         }
     })
     .then(()=> console.log("Successfully created application"))
     .catch((e)=> console.log('Application could not be saved: ' + e))
 
+}
+
+export async function getAllApplicationEntries(){
+    'use server'
+
+    try {
+        const applications = await db.application.findMany();
+        return applications
+    } catch (e) {
+        console.log("Could not find applications: " + e)
+        throw e
+    }  
+}
+
+export async function getApplicationById(id: number){
+    'use server'
+
+    try {
+        const application = await db.application.findFirst({
+            where: {
+                id: id
+            }
+        })
+        return application
+    } catch (e) {
+        console.log("Could not find entry")
+        throw e
+    }
 }
