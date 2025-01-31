52449a3046fefb1a8aea5361cb6d54e8536902da
OtmarKirch <154664034+OtmarKirch@users.noreply.github.com>
2025-01-31 16:59:47 +0100

    Develop application item

--- src/components/applicationItem.tsx
+++ src/components/applicationItem.tsx
@@ -1,4 +1,17 @@
-export default function ApplicationItem(){
+import { useState } from "react"
+import { getApplicationById } from '@/actions'
+
+interface itemProps {
+    id: number,
+}
+
+export default function ApplicationItem(props: itemProps){
+    const [applicationData, setApplicationData] = useState()
+
+    const dataRequest = getApplicationById(props.id)
+
+    setApplicationData(dataRequest)
+
     return (
         <div className="section-container">
             Application Item
